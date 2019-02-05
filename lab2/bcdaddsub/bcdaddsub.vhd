fb0aa6566e4bf1ea9a5d6608ac44afc3836f8e60
tomginsberg <tommyginsberg@gmail.com>
2019-02-05 15:40:04 -0800

    fixed fucntions

--- lab2/bcdaddsub/bcdaddsub.vhd
+++ lab2/bcdaddsub/bcdaddsub.vhd
@@ -43,65 +43,59 @@ architecture a of bcdaddsub is
 
     function bcdToSeven(bcd : std_logic_vector(3 downto 0))
     return std_logic_vector is
-
-        variable seven_seg : std_logic_vector(6 downto 0) := "000000";
-
         begin
             case bcd is
-                when "0000" => seven_seg <= "1111110"; --0
-                when "0001" => seven_seg <= "0110000"; --1 
-                when "0010" => seven_seg <= "1101101"; --2
-                when "0011" => seven_seg <= "1111001"; --3
-                when "0100" => seven_seg <= "0110011"; --4
-                when "0101" => seven_seg <= "1011011"; --5
-                when "0110" => seven_seg <= "1011111"; --6
-                when "0111" => seven_seg <= "1110000"; --7 
-                when "1000" => seven_seg <= "1111111"; --8
-                when "1001" => seven_seg <= "1110011"; --9
-                when others => seven_seg <= "0000000"; --no display
+                when "0000" => return "1111110"; --0
+                when "0001" => return "0110000"; --1 
+                when "0010" => return "1101101"; --2
+                when "0011" => return "1111001"; --3
+                when "0100" => return "0110011"; --4
+                when "0101" => return "1011011"; --5
+                when "0110" => return "1011111"; --6
+                when "0111" => return "1110000"; --7 
+                when "1000" => return "1111111"; --8
+                when "1001" => return "1110011"; --9
+                when others => return "0000000"; --no display
             end case;
-        return seven_seg;
         end;
 
 
     function addsub( bcd0 : std_logic_vector(3 downto 0); bcd1 : std_logic_vector(3 downto 0); carryin : std_logic; addTF : std_logic)
     return std_logic_vector is
 
-        variable bcd1comp : std_logic_vector(4 downto 0) := "00000";
-        variable sumtemp0 : std_logic_vector(4 downto 0) := "00000";
-        variable sumtemp1 : std_logic_vector(4 downto 0) := "00000";
-        variable numout : std_logic_vector(4 downto 0) := "00000";
+        variable bcd1comp : std_logic_vector(4 downto 0);
+        variable sumtemp0 : std_logic_vector(4 downto 0);
+        variable sumtemp1 : std_logic_vector(4 downto 0);
 
         begin
             if addTF = '0' then
 
                 case bcd1 is
-                    when "0000" => bcd1comp<="01001";
-                    when "0001" => bcd1comp<="01000";
-                    when "0010" => bcd1comp<="00111"; 
-                    when "0011" => bcd1comp<="00110";
-                    when "0100" => bcd1comp<="00101";
-                    when "0101" => bcd1comp<="00100";
-                    when "0110" => bcd1comp<="00011";
-                    when "0111" => bcd1comp<="00010";
-                    when "1000" => bcd1comp<="00001";
-                    when others => bcd1comp<="00000";
+                    when "0000" => bcd1comp:="01001";
+                    when "0001" => bcd1comp:="01000";
+                    when "0010" => bcd1comp:="00111"; 
+                    when "0011" => bcd1comp:="00110";
+                    when "0100" => bcd1comp:="00101";
+                    when "0101" => bcd1comp:="00100";
+                    when "0110" => bcd1comp:="00011";
+                    when "0111" => bcd1comp:="00010";
+                    when "1000" => bcd1comp:="00001";
+                    when others => bcd1comp:="00000";
                 end case;
 
             else
-                bcd1comp <= ('0' & bcd1);
+                bcd1comp := ('0' & bcd1);
             end if;
 
-            sumtemp0 <= ('0' & bcd0) + bcd1comp + ("0000" & carryin);
-            sumtemp1 <= sumtemp0 + "00110";
+            sumtemp0 := ('0' & bcd0) + bcd1comp + ("0000" & carryin);
+            sumtemp1 := sumtemp0 + "00110";
 
             if sumtemp0 > 9 then
-                numout <= ('1' & sumtemp1(3 downto 0));
+                return ('1' & sumtemp1(3 downto 0));
             else
-                numout <= sumtemp0;
+                return sumtemp0;
             end if;
 
-            return numout;
         end;
 
     begin
